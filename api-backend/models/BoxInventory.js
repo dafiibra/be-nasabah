129d5b83507be3bcbcbbb77c1372d3b94f9105e0
Ibrahim Dafi Iskandar <ibrahimdafi.id@gmail.com>
2026-03-04 13:56:32 +0700

    rapihin

--- api-backend/models/BoxInventory.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const boxInventorySchema = new mongoose.Schema({
-    box_size: { type: String, required: true, unique: true },
-    total_slots: { type: Number, default: 1700 }
-});
-
-const BoxInventory = mongoose.model('BoxInventory', boxInventorySchema);
-export default BoxInventory;
